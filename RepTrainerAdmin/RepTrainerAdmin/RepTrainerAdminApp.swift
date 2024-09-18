4157be31e275085c46533fe44d5da9fd67f0b261
ppoh71 <ppoh71@gmail.com>
2024-09-18 10:57:52 +0100

    Init

---
+++ RepTrainerAdmin/RepTrainerAdmin/RepTrainerAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepTrainerAdminApp.swift
+//  RepTrainerAdmin
+//
+//  Created by Peter Pohlmann on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RepTrainerAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
