2ba19c091f119cc414a4dc4c9b6715b73e8d4f1d
ppoh71 <ppoh71@gmail.com>
2024-09-19 14:55:01 +0100

    Admin app

--- RepTrainerAdmin/RepTrainerAdmin/RepTrainerAdminApp.swift
+++ RepTrainerAdmin/RepTrainerAdmin/RepTrainerAdminApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAppCheck
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+
+    /// Firewbase Debug, APPCheck not for LIVE !!!!
+    let providerFactory = AppCheckDebugProviderFactory()
+    AppCheck.setAppCheckProviderFactory(providerFactory)
+
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct RepTrainerAdminApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView().environmentObject(ObserverModel())
     }
+  }
 }
